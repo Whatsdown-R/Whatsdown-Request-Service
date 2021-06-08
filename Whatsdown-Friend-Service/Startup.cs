d7d2f7774fc1d585cd224b5d7b3cffc49531ffe8
Roel Lucassen <roel.lucassen@student.fontys.nl>
2021-06-08 10:45:15 +0200

    updated connection string

--- Whatsdown-Friend-Service/Startup.cs
+++ Whatsdown-Friend-Service/Startup.cs
@@ -26,7 +26,7 @@ namespace Whatsdown_Friend_Service
             services.AddControllers();
 
             services.AddDbContext<FriendContext>(options =>
-            options.UseMySql(Configuration.GetConnectionString("MvcMovieContext")));
+            options.UseMySql(Configuration.GetConnectionString("MessageDB")));
             services.AddControllers().AddNewtonsoftJson(options =>
             {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
